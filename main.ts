243d899a9fe0f07d5f29517d7e6940ca7286de3a
Aqsa Shahzad <160316789+Programer-aqsa@users.noreply.github.com>
2024-04-04 21:46:01 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+// 1)computer will generate a rendom number
+
+// 2)user input for gussing number
+
+// 3)compare user input with computer genrated number and show result
+let systemgenrateno =Math.floor (Math.random()*20);
+
+let answer:{
+    userguess:number;
+}=await inquirer.prompt({
+    type:"number",
+    name:"userguess",
+    message:"guess number between 1 to 20",
+});
+
+if(answer.userguess === systemgenrateno){
+    console.log("congratulation your guessd number is right");
+}else{
+    console.log(`opps!correct number was ${systemgenrateno} please try again`);
+}
